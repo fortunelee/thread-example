8f2dd70ec52e61fb590704bdb7e6234a3defe66c
lipeng <wb-lipeng@zhongan.io>
2018-09-14 15:06:48 +0800

    first commit

---
+++ src/main/java/com/thread/example/ThreadExampleApplication.java
@@ -0,0 +1,12 @@
+package com.thread.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadExampleApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(ThreadExampleApplication.class, args);
+    }
+}
