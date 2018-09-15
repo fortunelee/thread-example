55c1d15e486a206fdd8ae2fb561a2994f9426efc
lipeng <wb-lipeng@zhongan.io>
2018-09-15 14:59:49 +0800

    first commit

--- src/main/java/com/thread/example/executor/ThreadExample.java
+++ src/main/java/com/thread/example/executor/ThreadExample.java
@@ -1,8 +1,6 @@
 package com.thread.example.executor;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.Cache;
-import org.springframework.scheduling.concurrent.DefaultManagedAwareThreadFactory;
 
 import java.util.concurrent.*;
 
